d31bd4dec3fa8993aa0f9721c00b82e4c1d1c842
gautamkumar <gkt2work@gmail.com>
2025-04-24 19:10:34 +0530

    added-car-components

---
+++ src/car/car.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { CARS } from './cars.mock';
+
+@Injectable()
+export class CarService {
+
+    private cars = CARS;
+
+    public async getCars(){
+        return this.cars;
+
+    }
+
+}
---
+++ src/car/cars.mock.ts
@@ -0,0 +1,9 @@
+export const CARS =[
+    {
+        id: '16893246324',
+        brand: 'BMW',
+        color: 'gold',
+        model: 'BMW X5',
+    },
+
+];
