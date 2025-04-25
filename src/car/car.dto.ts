c27bb8913f8817e2bfddd859a88ee3508abf3c9b
gautamkumar <gkt2work@gmail.com>
2025-04-25 17:15:17 +0530

    added-car-dto

---
+++ src/car/car.dto.ts
@@ -0,0 +1,10 @@
+export class CarDto{
+    readonly id: number;
+    readonly brand: string;
+    readonly color: string;
+    readonly model: string;
+    readonly year: number;
+    readonly price: number;
+
+
+}
